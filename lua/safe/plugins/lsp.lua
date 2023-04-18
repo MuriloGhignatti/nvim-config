6c7fa571edadd8b730aa1e9968ae724606db86bd
Murilo Ghignatti <muriloghignatti@gmail.com>
2023-04-18 11:04:46 -0300

    Moving from coq_nvim to nvim-cmp

--- lua/safe/plugins/lsp.lua
+++ lua/safe/plugins/lsp.lua
@@ -26,45 +26,80 @@ return {
             }
         },
         dependencies = {
-            "williamboman/mason.nvim",
-            {
-                "neovim/nvim-lspconfig",
-                dependencies = {
-                    "ms-jpq/coq_nvim",
-                    dependencies = {
-                        "ms-jpq/coq.artifacts",
-                        config = function ()
-                            local lsp = require("lspconfig")
-                            local coq = require("coq")
-
-                            local lsp_attach = function (client, bufnr)
-                                
-                            end
-
-                            require('mason-lspconfig').setup_handlers({
-                                function(server_name)
-                                    lsp[server_name]
-                                    .setup(coq.lsp_ensure_capabilities(
-                                    {
-                                        on_attach = lsp_attach
-                                    }
-                                    ))
-                                end,
-                            })
-                        end
-                    }
+            "mason.nvim",
+            "neovim/nvim-lspconfig"
+        }
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        build = "make install_jsregexp"
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "nvim-lspconfig",
+            "LuaSnip",
+            "mason-lspconfig.nvim",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
+        },
+        config = function()
+            require("cmp").setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end
+                },
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" }
                 }
-            }
+            })
+            local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lsp_attach = function(client, bufnr)
+                local opts = { buffer = bufnr, remap = false }
+
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+            end
+
+            require('mason-lspconfig').setup_handlers({
+                function(server_name)
+                    require('lspconfig')[server_name].setup({
+                        capabilities = lsp_capabilities,
+                        on_attach = lsp_attach
+                    })
+                end
+            })
+        end
+    },
+    {
+        "saadparwaiz1/cmp_luasnip",
+        dependencies = {
+            "nvim-cmp"
         }
     },
     {
         "jay-babu/mason-null-ls.nvim",
         dependencies = {
-            "williamboman/mason.nvim",
-            "jose-elias-alvarez/null-ls.nvim",
-            config = true
+            "mason.nvim",
+            {
+                "jose-elias-alvarez/null-ls.nvim",
+                keys = {
+                    { "<leader>f", function() vim.lsp.buf.format() end }
+                },
+                config = true
+            }
         },
         config = true
     }
 }
-
