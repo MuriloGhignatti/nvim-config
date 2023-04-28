cb89a04a299392b153f9771ab759e32cb52753cf
Murilo Ghignatti <muriloghignatti@gmail.com>
2023-04-28 18:43:44 -0300

    Changing lspkind display mode to show text as well as the icon

--- lua/safe/plugins/lsp.lua
+++ lua/safe/plugins/lsp.lua
@@ -74,7 +74,7 @@ return {
                 },
                 formatting = {
                     format = require("lspkind").cmp_format({
-                        mode = 'symbol', -- show only symbol annotations
+                        mode = 'symbol_text', -- show only symbol annotations
                         maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
                         ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
                     })
