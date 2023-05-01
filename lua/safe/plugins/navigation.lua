462f84be55790fc972c92407fa2bc6d5009f27af
MuriloGhignatti <muriloghignatti@gmail.com>
2023-04-30 21:37:18 -0300

    Using cmake to build telescope-fzf

--- lua/safe/plugins/navigation.lua
+++ lua/safe/plugins/navigation.lua
@@ -7,7 +7,7 @@ return {
             "nvim-lua/plenary.nvim",
             {
                 "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
+                build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
                 config = function()
                     require("telescope").load_extension("fzf")
                 end
