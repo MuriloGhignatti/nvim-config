9ac42ca013ff1a19a61e4d472be1166d4cf3008d
Murilo Ghignatti <murilo.ghignatti@landisgyr.com>
2023-05-22 06:54:08 -0300

    Moving lsp_attach to be global instead of local

--- lua/safe/plugins/lsp.lua
+++ lua/safe/plugins/lsp.lua
@@ -1,3 +1,19 @@
+local lsp_attach = function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end
 return {
     {
         "williamboman/mason.nvim",
@@ -134,35 +150,19 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function(_, opts_lazy)
             local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lsp_attach = function(client, bufnr)
-                local opts = { buffer = bufnr, remap = false }
-
-                vim.keymap.set("n", "gD", function () vim.lsp.buf.declaration() end, opts)
-                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-            end
 
             local mason_lspconfig = require("mason-lspconfig")
             mason_lspconfig.setup(opts_lazy)
 
             mason_lspconfig.setup_handlers({
                 function(server_name)
-                    if server_name ~= "jdtls" then
-                        require('lspconfig')[server_name].setup({
-                            capabilities = lsp_capabilities,
-                            on_attach = lsp_attach
-                        })
-                    end
+                    require('lspconfig')[server_name].setup({
+                        capabilities = lsp_capabilities,
+                        on_attach = lsp_attach
+                    })
                 end,
+                ["jdtls"] = function ()
+                end
             })
         end
     },
@@ -356,7 +356,8 @@ return {
                         },
                         useBlocks = true,
                     },
-                }
+                },
+                on_attach = lsp_attach
             }
             jdtls.start_or_attach(config)
         end,
