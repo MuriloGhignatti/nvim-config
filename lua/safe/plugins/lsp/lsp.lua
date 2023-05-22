b9c7d6d8db32a823bc0ac14cfebd78959a623e1c
MuriloGhignatti <muriloghignatti@gmail.com>
2023-05-22 16:05:26 -0300

    Fixed cmp-nvim-lsp-document-symbol

--- lua/safe/plugins/lsp/lsp.lua
+++ lua/safe/plugins/lsp/lsp.lua
@@ -67,51 +67,56 @@ return {
     },
     config = function()
         local cmp = require("cmp")
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end
-                },
-                sources = {
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "nvim_lsp_signature_help" },
-                    { name = "calc" },
-                    { name = "nvim_lua" },
-                    { name = "buffer" }
-                },
-                formatting = {
-                    format = require("lspkind").cmp_format({
-                        mode = 'symbol_text',  -- show only symbol annotations
-                        maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-                    })
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end
+            },
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "nvim_lsp_signature_help" },
+                { name = "calc" },
+                { name = "nvim_lua" },
+                { name = "buffer" }
+            },
+            formatting = {
+                format = require("lspkind").cmp_format({
+                    mode = 'symbol_text',  -- show only symbol annotations
+                    maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                })
+            }
+        })
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp_document_symbol' }
+            }, {
+                { name = 'buffer' }
+            })
+        })
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                {
+                    name = 'cmdline',
+                    option = {
+                        ignore_cmds = { 'Man', '!' }
+                    }
                 }
             })
-            cmp.setup.cmdline('/', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({ {
-                    { name = 'nvim_lsp_document_symbol' }
-                }, {
-                    { name = 'buffer' }
-                } })
-            })
-
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' }
-                }, {
-                    {
-                        name = 'cmdline',
-                        option = {
-                            ignore_cmds = { 'Man', '!' }
-                        }
-                    }
-                })
-            })
-        end
-    }
+        })
+    end
+}
 }
