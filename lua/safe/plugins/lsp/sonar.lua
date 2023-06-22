cfc8c7df621e2eb439974968bb01067946ddefa1
Murilo Ghignatti <murilo.ghignatti@landisgyr.com>
2023-06-22 17:24:50 -0300

    Using path relative to mason-registry instead of conting on the MASON environment variable to be properly loaded

--- lua/safe/plugins/lsp/sonar.lua
+++ lua/safe/plugins/lsp/sonar.lua
@@ -2,20 +2,23 @@ return {
     url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
     ft = {"python", "cpp", "java"},
     dependencies = {
-        "mfussenegger/nvim-jdtls"
+        "mfussenegger/nvim-jdtls",
+        "williamboman/mason.nvim"
     },
     config = function()
-        local mason_path = os.getenv("MASON")
-        local mason_bin_path = mason_path .. "\\bin"
+        local sonar_language_server_path = require("mason-registry")
+            .get_package("sonarlint-language-server")
+            :get_install_path() 
+        local analyzers_path = sonar_language_server_path .. "/extension/analyzers"
         require("sonarlint").setup({
             server = {
                 cmd = {
-                    mason_bin_path .. "\\sonarlint-language-server.cmd",
+                    sonar_language_server_path .. "/sonarlint-language-server.cmd",
                     "-stdio",
                     "-analyzers",
-                    vim.fn.expand(mason_path .. "/share/sonarlint-analyzers/sonarpython.jar"),
-                    vim.fn.expand(mason_path .. "/share/sonarlint-analyzers/sonarcfamily.jar"),
-                    vim.fn.expand(mason_path .. "/share/sonarlint-analyzers/sonarjava.jar"),
+                    vim.fn.expand(analyzers_path .. "/sonarpython.jar"),
+                    vim.fn.expand(analyzers_path .. "/sonarcfamily.jar"),
+                    vim.fn.expand(analyzers_path .. "/sonarjava.jar"),
 
                 }
             },
