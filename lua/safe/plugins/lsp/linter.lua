c9c5c0636cd4c4b588a784e39ab44f043e3cc1f9
Murilo Ghignatti <muriloghignatti@gmail.com>
2023-07-31 07:53:30 -0300

    Removing sonarlint for now since it throws a lot of errors related to workspace directory

--- lua/safe/plugins/lsp/linter.lua
+++ lua/safe/plugins/lsp/linter.lua
@@ -8,37 +8,5 @@ return {
                 end
             })
         end
-    },
-    {
-        url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
-        ft = {"python", "cpp", "java"},
-        dependencies = {
-            "mfussenegger/nvim-jdtls",
-            "williamboman/mason.nvim"
-        },
-        config = function()
-            local sonar_language_server_path = require("mason-registry")
-                .get_package("sonarlint-language-server")
-                :get_install_path() 
-            local analyzers_path = sonar_language_server_path .. "/extension/analyzers"
-            require("sonarlint").setup({
-                server = {
-                    cmd = {
-                        sonar_language_server_path .. "/sonarlint-language-server.cmd",
-                        "-stdio",
-                        "-analyzers",
-                        vim.fn.expand(analyzers_path .. "/sonarpython.jar"),
-                        vim.fn.expand(analyzers_path .. "/sonarcfamily.jar"),
-                        vim.fn.expand(analyzers_path .. "/sonarjava.jar"),
-
-                    }
-                },
-                filetypes = {
-                    "python",
-                    "cpp",
-                    "java"
-                }
-            })
-        end
     }
 }
