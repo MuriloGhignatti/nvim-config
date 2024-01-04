3a24041852182e534b04f04ae338cb4b46312a28
MuriloGhignatti <muriloghignatti@gmail.com>
2024-01-03 22:32:48 -0300

    Added used formatters for auto install

--- lua/safe/plugins/lsp/mason.lua
+++ lua/safe/plugins/lsp/mason.lua
@@ -1,49 +1,59 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = true,
-        build = ":MasonUpdate"
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-                "clangd",
-                "cmake",
-                "dockerls",
-                "gradle_ls",
-                "html",
-                "cssls",
-                "jsonls",
-                "jdtls",
-                "tsserver",
-                "kotlin_language_server",
-                "lua_ls",
-                "pylsp",
-                "sqlls",
-                "yamlls",
-                "lemminx"
-            }
-        },
-        dependencies = {
-            "williamboman/mason.nvim",
-            "hrsh7th/nvim-cmp"
-        },
-        config = function(_, opts_lazy)
-            local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = true,
+		build = ":MasonUpdate",
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"clangd",
+				"cssls",
+				"cmake",
+				"dockerls",
+				"gradle_ls",
+				"html",
+				"jsonls",
+				"jdtls",
+				"kotlin_language_server",
+				"lemminx",
+				"lua_ls",
+				"pylsp",
+				"sqlls",
+				"tsserver",
+				"yamlls",
 
-            local mason_lspconfig = require("mason-lspconfig")
-            mason_lspconfig.setup(opts_lazy)
+				-- Formatters
+				"beautysh",
+				"black",
+				"clang_format",
+				"gofumpt",
+				"rustfmt",
+				"stylua",
+				"prettierd",
+				"xmlformat",
+			},
+		},
+		dependencies = {
+			"williamboman/mason.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		config = function(_, opts_lazy)
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            mason_lspconfig.setup_handlers({
-                function(server_name)
-                    if server_name ~= "jdtls" then
-                        require('lspconfig')[server_name].setup({
-                            capabilities = lsp_capabilities,
-                        })
-                    end
-                end
-            })
-        end
-    }
+			local mason_lspconfig = require("mason-lspconfig")
+			mason_lspconfig.setup(opts_lazy)
+
+			mason_lspconfig.setup_handlers({
+				function(server_name)
+					if server_name ~= "jdtls" then
+						require("lspconfig")[server_name].setup({
+							capabilities = lsp_capabilities,
+						})
+					end
+				end,
+			})
+		end,
+	},
 }
