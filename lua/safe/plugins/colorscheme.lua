00ea5b369ee9983bb0a5e8065da14c6305f195f2
Murilo Ghignatti <murilo.ghignatti@landisgyr.com>
2024-04-02 15:05:58 -0300

    Changed colorscheme to rose-pine

--- lua/safe/plugins/colorscheme.lua
+++ lua/safe/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
-	"rebelot/kanagawa.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {
-		transparent = true,
+	-- {
+	-- "rebelot/kanagawa.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.cmd("colorscheme kanagawa-dragon")
+	-- end,
+	-- }
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		config = function(opts)
+			require("rose-pine").setup({
+				disable_background = true,
+			})
+			vim.cmd("colorscheme rose-pine")
+		end,
 	},
-	config = function(opts)
-		require("kanagawa").setup(opts)
-		vim.cmd("colorscheme kanagawa-dragon")
-	end,
 }
