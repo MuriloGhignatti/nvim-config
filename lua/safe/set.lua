34bb9897d40789b2befd70ddbe9116f0b0136482
Murilo Ghignatti <muriloghignatti@gmail.com>
2024-04-24 11:37:12 -0300

    Adding neovide configuration for better use of nvim on windows

--- lua/safe/set.lua
+++ lua/safe/set.lua
@@ -1,1 +1,7 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "[P]roject View" })
+
+if vim.g.neovide then
+    vim.o.guifont = "JetBrainsMono Nerd Font:h14" -- text below applies for VimScript
+    vim.g.neovide_transparency = 0.8
+    vim.g.neovide_fullscreen = true
+end
