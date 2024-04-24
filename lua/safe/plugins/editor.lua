dc5445c3738e41b0d45d4643ca3aee2b722c79c6
Murilo Ghignatti <muriloghignatti@gmail.com>
2024-04-24 11:36:48 -0300

    Adding a dashboard in case I open no files while running nvim

--- lua/safe/plugins/editor.lua
+++ lua/safe/plugins/editor.lua
@@ -79,4 +79,14 @@ return {
 		end,
 		ft = { "markdown" },
 	},
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({
+				-- config
+			})
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
 }
