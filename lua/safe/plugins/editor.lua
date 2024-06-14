a63f188f29196c7329b590464414cfe0c2315562
Murilo Ghignatti <muriloghignatti@gmail.com>
2024-06-14 11:12:51 -0300

    Moved markdown previewer to it's own lua file in case more previewers are added

--- lua/safe/plugins/editor.lua
+++ lua/safe/plugins/editor.lua
@@ -72,15 +72,6 @@ return {
 		config = true,
 	},
 	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && npm install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-	},
-	{
 		"nvimdev/dashboard-nvim",
 		event = "VimEnter",
 		config = function()
