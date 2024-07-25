d8fa3f295d672c257a17810909e742eb861a481d
Murilo Ghignatti <muriloghignatti@gmail.com>
2024-07-25 19:07:21 -0300

    Changing which-key to use the new add function

--- lua/safe/plugins/editor.lua
+++ lua/safe/plugins/editor.lua
@@ -41,13 +41,13 @@ return {
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]rouble", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>t", group = "[T]rouble" },
 			})
 		end,
 	},
@@ -81,7 +81,16 @@ return {
 		end,
 		dependencies = { { "nvim-tree/nvim-web-devicons" } },
 	},
-    {
-        "lambdalisue/suda.vim"
-    }
+	{
+		"lambdalisue/suda.vim",
+	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		dependencies = "nvim-tree/nvim-web-devicons",
+		keys = {
+			{ "<leader>o", "<CMD>Oil<CR>", desc = "[O]il navigation" },
+			{ "<leader>pv", "<CMD>Oil<CR>", desc = "[P]roject [V]iew" },
+		},
+	},
 }
