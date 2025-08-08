28b6e2ef72673256e4fe48048cd51d647e1de0f0
Murilo Ghignatti <muriloghignatti@gmail.com>
2025-08-08 14:09:00 -0300

    Fixing identation and adding nvim-java configuration

--- lua/safe/plugins/lsp.lua
+++ lua/safe/plugins/lsp.lua
@@ -1,11 +1,11 @@
 return {
-    {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        commit = "93a9ff9",
-        opts = {
-            ensure_installed = {
-                -- Servers
-                "lua_ls",
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		commit = "93a9ff9",
+		opts = {
+			ensure_installed = {
+				-- Servers
+				"lua_ls",
 				"clangd",
 				"cssls",
 				"neocmake",
@@ -23,72 +23,131 @@ return {
 				"beautysh",
 				"black",
 				"clang-format",
-                { "gofumpt", condition = function () return vim.fn.executable('go') == 1 end },
+				{
+					"gofumpt",
+					condition = function()
+						return vim.fn.executable("go") == 1
+					end,
+				},
 				"prettierd",
 				"stylua",
 				"xmlformatter",
 
 				-- Debug
 				"debugpy",
-            }
-        },
-        dependencies = {
-            {
-                "mason-org/mason-lspconfig.nvim",
-                version = "2.0.0",
-                opts = {}
-            },
-            { "mason-org/mason.nvim", version = "2.0.0", opts = {} },
-            {
-                "jay-babu/mason-nvim-dap.nvim",
-                version = "2.5.1",
-                opts = {},
-            },
-            { "neovim/nvim-lspconfig", version = "2.2.0" }
-        }
-    },
-        	-- Useful status updates for LSP.
-        	-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-        	{
-        		"j-hui/fidget.nvim",
-        		version = "1.5.0",
-                opts = {},
-        	},
-            {
-        		"folke/trouble.nvim",
-        		version = "3.7.1",
-                cmd = "Trouble",
-                keys = {
-                    {
-                      "<leader>xx",
-                      "<cmd>Trouble diagnostics toggle<cr>",
-                      desc = "Diagnostics (Trouble)",
-                    },
-                    {
-                      "<leader>xX",
-                      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-                      desc = "Buffer Diagnostics (Trouble)",
-                    },
-                    {
-                      "<leader>cs",
-                      "<cmd>Trouble symbols toggle focus=false<cr>",
-                      desc = "Symbols (Trouble)",
-                    },
-                    {
-                      "<leader>cl",
-                      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-                      desc = "LSP Definitions / references / ... (Trouble)",
-                    },
-                    {
-                      "<leader>xL",
-                      "<cmd>Trouble loclist toggle<cr>",
-                      desc = "Location List (Trouble)",
-                    },
-                    {
-                      "<leader>xQ",
-                      "<cmd>Trouble qflist toggle<cr>",
-                      desc = "Quickfix List (Trouble)",
-                    },
-                  },
-            }
-    }
+			},
+		},
+		dependencies = {
+			{
+				"mason-org/mason-lspconfig.nvim",
+				version = "2.0.0",
+				opts = {
+					automatic_enable = {
+						exclude = {
+							"jdtls",
+						},
+					},
+				},
+			},
+			{ "mason-org/mason.nvim", version = "2.0.0", opts = {} },
+			{
+				"jay-babu/mason-nvim-dap.nvim",
+				version = "2.5.1",
+				opts = {},
+			},
+			{
+				"neovim/nvim-lspconfig",
+				version = "2.2.0",
+				config = function()
+					vim.lsp.config("jdtls", {
+						settings = {
+							java = {
+								configuration = {
+									runtimes = {
+										{
+											name = "JavaSE-21",
+											path = "/usr/lib/jvm/java-21-openjdk",
+											default = true,
+										},
+										{
+											name = "JavaSE-17",
+											path = "/usr/lib/jvm/java-17-openjdk",
+											default = false,
+										},
+										{
+											name = "JavaSE-11",
+											path = "/usr/lib/jvm/java-11-openjdk",
+											default = false,
+										},
+									},
+								},
+							},
+						},
+					})
+				end,
+			},
+		},
+	},
+	{
+		"nvim-java/nvim-java",
+		version = "3.0.0",
+		opts = {
+			root_markers = {
+				"settings.gradle",
+				"settings.gradle.kts",
+				"pom.xml",
+				"module.ivy",
+				"build.gradle",
+				"mvnw",
+				"gradlew",
+				"build.gradle",
+				"build.gradle.kts",
+				".git",
+			},
+		},
+	},
+	-- Useful status updates for LSP.
+	-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+	{
+		"j-hui/fidget.nvim",
+		version = "1.5.0",
+		opts = {},
+	},
+	{
+		"folke/trouble.nvim",
+		version = "3.7.1",
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+}
