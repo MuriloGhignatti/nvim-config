5a2dd6eaf774a4d5429b9f64c3a484a3d173d9be
Murilo Ghignatti <muriloghignatti@gmail.com>
2025-08-08 14:09:20 -0300

    Trying snacks instead of mini for file picker

--- lua/safe/plugins/mini.lua
+++ lua/safe/plugins/mini.lua
@@ -21,15 +21,15 @@ return {
 			require("mini.surround").setup()
 
 			-- Activate animations in case we are not using neovide
-			if not vim.g.neovide then
-				require("mini.animate").setup()
-			end
+			-- if not vim.g.neovide then
+			-- 	require("mini.animate").setup()
+			-- end
 
 			-- Automatically add pairs
 			require("mini.pairs").setup()
 
 			-- Picker similar to telescope
-			require("mini.pick").setup()
+			-- require("mini.pick").setup()
 
 			-- Code completion, substitutes nvim-cmp
 			require("mini.completion").setup()
@@ -42,7 +42,7 @@ return {
 
 			require("mini.git").setup()
 			require("mini.diff").setup()
-			require("mini.statusline").setup()
+			-- require("mini.statusline").setup()
 
 			-- Comments
 			--
@@ -51,57 +51,57 @@ return {
 			require("mini.comment").setup()
 			require("mini.extra").setup()
 		end,
-		keys = {
-			{
-				"<leader>sf",
-				function()
-					MiniPick.builtin.files()
-				end,
-				desc = "[S]earch [F]iles",
-			},
-			{
-				"<leader>sh",
-				function()
-					MiniPick.builtin.help()
-				end,
-				desc = "[S]earch [H]elp",
-			},
-			{
-				"<leader>sk",
-				function()
-					MiniPick.builtin.keymaps()
-				end,
-				desc = "[S]earch [K]eymaps",
-			},
-			{
-				"<leader>sg",
-				function()
-					MiniPick.builtin.grep_live()
-				end,
-				desc = "[S]earch [G]rep",
-			},
-			{
-				"<leader>sd",
-				function()
-					MiniExtra.pickers.diagnostic()
-				end,
-				desc = "[S]earch [D]iagnostics",
-			},
-			{
-				"<leader>sr",
-				function()
-					MiniPick.builtin.resume()
-				end,
-				desc = "[S]earch [R]esume",
-			},
-			{
-				"<leader><leader>",
-				function()
-					MiniPick.builtin.buffers()
-				end,
-				desc = "Find existing buffers",
-			},
-		},
+		-- keys = {
+		-- 	{
+		-- 		"<leader>sf",
+		-- 		function()
+		-- 			MiniPick.builtin.files()
+		-- 		end,
+		-- 		desc = "[S]earch [F]iles",
+		-- 	},
+		-- 	{
+		-- 		"<leader>sh",
+		-- 		function()
+		-- 			MiniPick.builtin.help()
+		-- 		end,
+		-- 		desc = "[S]earch [H]elp",
+		-- 	},
+		-- 	{
+		-- 		"<leader>sk",
+		-- 		function()
+		-- 			MiniPick.builtin.keymaps()
+		-- 		end,
+		-- 		desc = "[S]earch [K]eymaps",
+		-- 	},
+		-- 	{
+		-- 		"<leader>sg",
+		-- 		function()
+		-- 			MiniPick.builtin.grep_live()
+		-- 		end,
+		-- 		desc = "[S]earch [G]rep",
+		-- 	},
+		-- 	{
+		-- 		"<leader>sd",
+		-- 		function()
+		-- 			MiniExtra.pickers.diagnostic()
+		-- 		end,
+		-- 		desc = "[S]earch [D]iagnostics",
+		-- 	},
+		-- 	{
+		-- 		"<leader>sr",
+		-- 		function()
+		-- 			MiniPick.builtin.resume()
+		-- 		end,
+		-- 		desc = "[S]earch [R]esume",
+		-- 	},
+		-- 	{
+		-- 		"<leader><leader>",
+		-- 		function()
+		-- 			MiniPick.builtin.buffers()
+		-- 		end,
+		-- 		desc = "Find existing buffers",
+		-- 	},
+		-- },
 		dependencies = {
 			"rafamadriz/friendly-snippets",
 			commit = "572f566",
