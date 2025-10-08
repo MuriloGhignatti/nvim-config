759ec31b0965bad608e261832dc09e20b501bc0f
Murilo Ghignatti <murilo.ghignatti@landisgyr.com>
2025-10-08 18:16:06 -0300

    Fixing tree-sitter extra functionality and also adding .ivy and .ant to the xml parser

--- lua/safe/plugins/treesitter.lua
+++ lua/safe/plugins/treesitter.lua
@@ -6,8 +6,9 @@ return {
 	commit = "5a70b1e",
 	config = function()
 		require("nvim-treesitter").install({ "bash", "c", "java", "lua", "markdown", "xml", "markdown_inline" })
+        vim.treesitter.language.register('xml', { 'ivy', 'ant' })
 		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "<filetype>" },
+			pattern = require("nvim-treesitter").get_installed(),
 			callback = function()
 				vim.treesitter.start()
 				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
