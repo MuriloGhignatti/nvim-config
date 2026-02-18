0d3ca3b2b763c6a19f3fe31f0f2622c21a2a1990
Murilo Henrique Ghignatti <murilo@Hogger.localdomain>
2026-02-17 22:38:35 -0300

    Fixing filter used in treesitter features

--- lua/safe/plugins/treesitter.lua
+++ lua/safe/plugins/treesitter.lua
@@ -3,14 +3,27 @@ return {
 	lazy = false,
 	branch = "main",
 	build = ":TSUpdate",
-	commit = "5a70b1e",
+	commit = "2bd9b9b",
 	config = function()
-		require("nvim-treesitter").install({ "bash", "c", "java", "lua", "markdown", "xml", "markdown_inline" })
-        vim.treesitter.language.register('xml', { 'ivy', 'ant' })
+		local ts = require("nvim-treesitter")
+		ts.install({ "bash", "c", "java", "lua", "markdown", "xml", "markdown_inline" })
+
+		vim.treesitter.language.register("xml", { "ivy", "ant" })
+
+		local ts_filetypes = vim.iter(ts.get_installed())
+			:map(function(lang)
+				return vim.treesitter.language.get_filetypes(lang)
+			end)
+			:flatten()
+			:totable()
+
 		vim.api.nvim_create_autocmd("FileType", {
-			pattern = require("nvim-treesitter").get_installed(),
-			callback = function()
-				vim.treesitter.start()
+			desc = "Setup treesitter for a buffer",
+			pattern = ts_filetypes,
+			group = vim.api.nvim_create_augroup("ts_setup", { clear = true }),
+			callback = function(e)
+				vim.treesitter.start(e.buf)
+				vim.wo.foldmethod = "expr"
 				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 			end,
